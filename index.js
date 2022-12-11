2735be45e10964e280e288712093ef07231623a7
手瓜一十雪 <nineto0@163.com>
2022-12-11 12:45:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var AppEnv = {}
 AppEnv.NodeVer = process.version.substr(1).split(".");
 AppEnv.AppVer = "1.0.0".split(".");
 //测试Github网络
-if(Number(AppEnv.NodeVer[-1])<19){console.log("Please Update NodeJs");process.exit(1);}
-console.log(AppEnv.NodeVer[0]);
+if(Number(AppEnv.NodeVer[0])<19){console.log("Please Update NodeJs");process.exit(1);}
+console.log(AppEnv.NodeVer);
 //需要先检测环境 再设置环境变量和配置
