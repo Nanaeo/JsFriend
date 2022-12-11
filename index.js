0ced7a263c473b654cbc0518fc3243c7b8b0962a
手瓜一十雪 <nineto0@163.com>
2022-12-11 12:09:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let fs = require("fs");
 let path = require("path");
 //Loading
 // 检测Nodejs版本 当前网络状态
-var string = process.version.substr(1); 
-console.log(string);
+var AppEnv = {"NodeVer":process.version.substr(1).split(".")}
+console.log(AppEnv);
 //需要先检测环境 再设置环境变量和配置
